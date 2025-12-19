44831f6aeee986a86f9fffc672f38801c99520dd
shaelynmugan <sjmugan@gmail.com>
2025-12-19 17:58:24 -0600

    dec 19

---
+++ fixt-website/src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HomeCarousel from '@/pages/HomeCarousel.vue'
+</script>
+
+<template>
+  <section>
+    <h1>Home</h1>
+    <p>placeholderrrrr</p>
+    <HomeCarousel />
+  </section>
+</template>
